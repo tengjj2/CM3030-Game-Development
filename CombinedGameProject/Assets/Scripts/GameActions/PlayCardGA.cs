c33097bb1055d830a9acca2eabe618b81f349883
XTheXuan <161691680+XTheXuan@users.noreply.github.com>
2025-08-27 19:13:22 +0800

    Add files via upload

---
+++ CombinedGameProject/Assets/Scripts/GameActions/PlayCardGA.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayCardGA : GameAction
+{
+    public EnemyView ManualTarget { get; set; }
+    public Card Card { get; set; }
+    public PlayCardGA(Card card)
+    {
+        Card = card;
+        ManualTarget = null;
+    }
+
+    public PlayCardGA(Card card, EnemyView target)
+    {
+        Card = card;
+        ManualTarget = target;
+    }
+}
