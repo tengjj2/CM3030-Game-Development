249202b34dafb09a2bd2c65ffc059959e7ba3736
zi-han28 <xu_zihan@outlook.com>
2025-08-30 12:10:44 +0800

    new commit

--- Assets/Scripts/Views/TargetArrowView.cs
+++ Assets/Scripts/Views/TargetArrowView.cs
@@ -8,11 +8,7 @@ public class TargetArrowView : MonoBehaviour
     private Vector3 startPositon;
     private void Update()
     {
-        if (Camera.main == null) return;
-
         Vector3 endPosition = MouseUtil.GetMousePositionInWorldSpace();
-        if (endPosition == Vector3.zero) return;
-
         Vector3 direction = -(startPositon - arrowHead.transform.position).normalized;
         lineRenderer.SetPosition(1, endPosition - direction * 0.5f);
         arrowHead.transform.position = endPosition;
@@ -22,12 +18,6 @@ public class TargetArrowView : MonoBehaviour
     {
         this.startPositon = startPosition;
         lineRenderer.SetPosition(0, startPosition);
-
-        // lineRenderer.SetPosition(1, MouseUtil.GetMousePositionInWorldSpace());
-        Vector3 mousePosition = MouseUtil.GetMousePositionInWorldSpace();
-        if (mousePosition != Vector3.zero) // Added null check
-        {
-            lineRenderer.SetPosition(1, mousePosition);
-        }
+        lineRenderer.SetPosition(1, MouseUtil.GetMousePositionInWorldSpace());
     }
 }
